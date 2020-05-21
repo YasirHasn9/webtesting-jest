58eeef974805abfd41e48b4aae32d53e71053757
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-21 15:29:25 -0700

    repair is passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+// - a `repair(item)` method that accepts an `item`
+//  object and **returns a new
+//  item** with the durability restored to 100.
+//  This method is the simplest of the three and would be a
+// - Items have `name`, `durability` and `enhancement`.
+// - The item's `enhancement` it's a number from 0 to 20.
+// - The item's `durability` it's a number from 0 to 100.
+describe("repair", () => {
+  it("returns a new item with the durability restored to 100.", () => {
+    const repairItem = {
+      name: "Yasir",
+      enhancement: 18,
+      durability: 100
+    };
+    const expected = enhancer.repair(repairItem);
+    expect(expected.durability).toBe(100);
+  });
+});
